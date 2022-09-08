7e7141c30a1af4fa43a344db7327ae5996ec93b1
Alok <alokroy0896@gmail.com>
2022-09-08 17:30:17 +0530

    Dynamic carousel card

--- carouselCardDemo.js
+++ carouselCardDemo.js
@@ -1,3 +1,33 @@
 import { LightningElement } from 'lwc';
 
-export default class CarouselCardDemo extends LightningElement {}
+export default class CarouselCardDemo extends LightningElement {
+ student = [
+    {
+     id : "1",
+     src : "https://media.designrush.com/agency_photos_and_videos/20276/conversions/Algoworks-Logo-800x480-thumb.jpg",
+     header : "CLASS ROOM",
+     description : " Test Description",
+     href : "https://www.algoworks.com/company/"  
+    },
+    {
+        id : "2",
+        src : "https://liveimages.algoworks.com/new-algoworks/wp-content/uploads/2021/07/13125820/algoworks-featured-on-top-design-firms.png",
+        header : "Algoworks Technologies",
+        description : "Algoworks is an IT service provider recognized globally as a technology expert with offices in  Sunnyvale, California; Toronto, Canada; and Noida, India. Started in 2006,Algoworks has a the customer retention rate stands at a whopping 99% for customers from US, UK, Europe, East Asia, South America, and the Middle-East.The company has a workforce of around 630+ people worldwide. ",
+        href : "https://www.algoworks.com/company/" 
+       },
+       {
+        id : "3",
+        src : "https://www.prlog.org/12627094-holi2017.jpg",
+        header : "Colleagues",
+        description : " Test Description",
+        href : "https://www.algoworks.com/company/" 
+       },
+
+
+ ]
+
+  
+
+
+}
